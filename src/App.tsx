dea6036f80ba0a99f0cca2dc360d434f2ca29a55
kiencx <kiencx@appota.com>
2023-12-28 11:05:46 +0700

    f1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
+  const [inputData, setInputData] = useState({
+    title: "",
+    message: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    // Handle sending data to the app
+    const dataToSend = JSON.stringify(inputData);
+    window.location.href = `AwesomeProject://open-app?data=${encodeURIComponent(
+      dataToSend
+    )}`;
+    // You may modify the URL structure based on how your app expects data
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100vh",
+        alignItems: "center",
+      }}
+    >
+      <input
+        type="text"
+        name="title"
+        value={inputData.title}
+        placeholder="Title"
+        onChange={handleInputChange}
+        style={{ marginBottom: "10px" }}
+      />
+      <input
+        type="text"
+        name="message"
+        value={inputData.message}
+        placeholder="Message"
+        onChange={handleInputChange}
+        style={{ marginBottom: "10px" }}
+      />
+      <button onClick={handleSubmit}>Gửi Dữ Liệu</button>
     </div>
   );
 }
-
 export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
